f36e51bdfa4ce9cf39e9a752f6e384d2cd36415e
KETAMINION <106491264+KETAMINION@users.noreply.github.com>
2023-06-30 12:47:06 +0100

    fixing same issue

--- models/databaseModels.js
+++ models/databaseModels.js
@@ -326,7 +326,7 @@ async function getAllUserContent() {
     const sqlQuery = `SELECT * FROM userContent WHERE purpose='userContent';`;
     const result = await pool.query(sqlQuery);
     let links = result.rows;
-    links = links.map(userContentMap);
+    // links = links.map(userContentMap);
     return links;
   }catch(error){
     console.error(error)
